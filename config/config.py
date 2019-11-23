e23264042b040d4b51f2d44699d58311e94b2f5d
Pavlo Chernyshev <pchernyshev@gmail.com>
2019-11-23 22:46:59 +0200

    Mock for configuration
    Signed-off-by: Pavlo Chernyshev <pchernyshev@gmail.com>

---
+++ config/config.py
@@ -0,0 +1,13 @@
+#######################  Telegram Bot Config #######################
+
+BOT_TOKEN='000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' # the token you get from botfather
+
+####################################################################
+
+
+#######################  Google Drive Config #######################
+
+GDRIVE_CRED='config/client_secret.json' #This is the name of the file you download from the console.developers.google.com/apis
+GDRIVE_URL='https://docs.google.com/spreadsheets/d/11111111111111111111111111111111111111111111/'
+
+####################################################################

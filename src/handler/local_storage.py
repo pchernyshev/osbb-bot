8556c4d245feba6ea780e937bc5c5b158d7dfed7
Pavlo Chernyshev <pchernyshev@gmail.com>
2019-11-28 03:22:29 +0200

    Typo

--- src/handler/local_storage.py
+++ src/handler/local_storage.py
@@ -5,11 +5,10 @@ from src.handler.const import AuthStates, START_AUTHORIZED
 class Client:
     def __init__(self, phone="", house="", apt=0,
                  authorized: bool = START_AUTHORIZED):
-        self.auth_state = AuthStates.AUTHORIZED_STATE\
-            if START_AUTHORIZED else AuthStates.UNAUTHORIZED_STATE
-        self.phone = ""
-        self.house = ""
-        self.apt = 0
+        self.auth_state = authorized
+        self.phone = phone
+        self.house = house
+        self.apt = apt
 
     @classmethod
     def from_context(cls, context):
